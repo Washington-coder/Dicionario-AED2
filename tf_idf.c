82c70642984eddad11ade5f1e12bedb11c095920
Amanda-Spellen <amandaspellen123@gmail.com>
2023-02-03 18:52:51 -0400

    Adaptação de nomes no TF-IDF, comentários explicativos

--- tf_idf.c
+++ tf_idf.c
@@ -4,6 +4,7 @@
 #include "leitura_pag.h"
 #include "dicio_semi_dinamico.h"
 
+// Tipo contendo o dicionário de palavras, com suas ocorrências e páginas
 typedef struct sLivro{
     DicioSemiDinamico* frequencia_livro; // uh oh (pointer?)
     TLivro* livro; // uh oh 2
@@ -11,24 +12,24 @@ typedef struct sLivro{
 
 
 // retorna a frequência do termo em um documento
-long tf(char* palavra, FrequenciaLivro* livro){
+long tf(char* palavra, FrequenciaLivro* pagina){
 
 }
 
 // retorna a quantidade de documentos que contém um termo
 // necessário?
-long n_containing(char* palavra, FrequenciaLivro** livros){
+long n_containing(char* palavra, FrequenciaLivro** livro){
 
 }
 
 // retorna o quão comum é uma palavra em uma série de documentos
 // necessário?
-long idf(char* palavra, FrequenciaLivro** livros){
+long idf(char* palavra, FrequenciaLivro** livro){
 
 }
 
 // retorna o TF-IDF
 // necessário?
-long tfidf(char* palavra, FrequenciaLivro* livro, FrequenciaLivro** livros){
-    return tf(palavra, livro) * idf(palavra, livros);
+long tfidf(char* palavra, FrequenciaLivro* pagina, FrequenciaLivro** livro){
+    return tf(palavra, pagina) * idf(palavra, livro);
 }
